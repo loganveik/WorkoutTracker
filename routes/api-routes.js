5181e5288c8b75b8ccb0ade618587ddea7717b94
ljv29 <jumpswervelogan@gmail.com>
2020-04-09 03:19:29 -0700

    basic working workout tracker using mongodb. some bugs still need to be fixed

---
+++ routes/api-routes.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workout", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    { _id: params.id },
+    {
+      $push: {
+        exercises: body
+      }
+    }).then(dbWorkout => {
+      res.json(dbWorkout)
+    }).catch(err => {
+      res.status(400).json(err);
+    })
+})
+
+router.get("/api/workouts/range", function (req, res) {
+  Workout.find({})
+    .limit(5)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+
+module.exports = router;
